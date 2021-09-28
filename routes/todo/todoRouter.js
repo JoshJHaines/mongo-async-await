af0a568307fce5e975bb290e2e912f691f244dfd
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-28 11:14:50 -0400

    imported and called fetchTodo function

--- routes/todo/todoRouter.js
+++ routes/todo/todoRouter.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+const { fetchTodo } = require("./controller/todoController")
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-	res.json({ message: "index" });
-});
+router.get("/", fetchTodo)
 
 module.exports = router;
