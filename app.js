c5791a4fbf54e708f84b69c80ad1a79528bacc7e
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-28 10:51:31 -0400

    adding todo route to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var todoRouter = require('./routes/todo/todoRouter');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/todo', todoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
